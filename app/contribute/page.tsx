abfc2f84d215c012e9ae038682fb88f0e422899b
LGC <lguangcong@163.com>
2025-02-16 21:01:09 +0800

    feat: import @emotion/styled

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -5,6 +5,7 @@ export default function ContributePage() {
 		<div className="min-h-screen">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
 				<h1 className="text-4xl font-bold font-special">Contribute</h1>
+				<h1 className="text-4xl font-bold">Contribute</h1>
 				<p className="mt-4 text-gray-600">
 					Propose projects, participate with the community to verify on proposals, then
 					contribute on project pages and gain rewards!
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { Button } from '@heroui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import styled from '@emotion/styled'
 
 export default function Page() {
 	return (
 		<div className="p-4">
 			<Button color="primary">Primary Button</Button>
+			<CustomButton className="text-white rounded-lg ml-1 bg-blue-500">CustomButton</CustomButton>
 			<div className="bg-blue-500 text-white p-4 rounded-lg mt-4">
 				This div should have a blue background if Tailwind is working
 			</div>
@@ -26,3 +28,8 @@ export default function Page() {
 		</div>
 	);
 }
+
+const CustomButton = styled(Button)({
+	width: '400px',
+	backgroundColor: 'red'
+})
