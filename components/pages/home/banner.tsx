43650ee00a8e937e7d611a047a96fcc7d94e01a8
LGC <lguangcong@163.com>
2025-02-20 21:22:03 +0800

    feat: update ui

--- components/pages/home/banner.tsx
+++ components/pages/home/banner.tsx
@@ -7,10 +7,10 @@ const Banner = () => {
 	return (
 		<div
 			className={cn(
-				'w-full h-[167px] overflow-hidden bg-white rounded-[10px] border border-[rgba(0,0,0,0.1)] p-5 relative bg-[url(/images/home/BannerBg.png)] bg-no-repeat ',
-				'mobile:h-[auto] mobile:bg-none',
+				'w-full h-[167px] mobile:h-[auto] overflow-hidden bg-white rounded-[10px] border border-[rgba(0,0,0,0.1)] p-5 relative ',
+				'bg-[url(/images/home/BannerBg.png)] bg-no-repeat bg-right bg-[length:auto_100%]',
+				'mobile:bg-none',
 			)}
-			style={{ backgroundPosition: 'right center', backgroundSize: 'auto 100%' }}
 		>
 			<div className="w-full h-full z-10">
 				<div className="flex h-[25px]">
@@ -19,20 +19,27 @@ const Banner = () => {
 					</p>
 				</div>
 
-				<ECFTypography type={'title'} className="mt-2.5">
+				<ECFTypography
+					type={'title'}
+					className="mt-2.5 mobile:text-[30px] mobile:leading-[42px]"
+				>
 					Welcome to{' '}
 					<span className="text-[var(--primary-green)]">ecf.network,</span>{' '}
 				</ECFTypography>
 
-				<ECFTypography type={'subtitle2'} className="mt-2.5">
+				<ECFTypography
+					type={'subtitle2'}
+					className="mt-2.5 mobile:text-base mobile:leading-[26px]"
+				>
 					A community-curated and governed platform to discover credible web3 projects.
 				</ECFTypography>
 			</div>
 
 			<div
 				className={cn(
-					'lg:hidden pc:hidden tablet:hidden absolute top-[28px] left-[178px] w-[500px] h-[300px] opacity-20 z-5',
-					'bg-[url(/images/home/Cube.png)] bg-repeat bg-[length:100px_100px] bg-white grayscale-25',
+					'lg:hidden pc:hidden tablet:hidden',
+					'absolute top-[28px] left-[178px] bottom-0 right-0 opacity-40',
+					'bg-[url(/images/home/Cube.png)] bg-repeat mobile:bg-[length:100px_100px] mix-blend-luminosity',
 				)}
 			/>
 		</div>
