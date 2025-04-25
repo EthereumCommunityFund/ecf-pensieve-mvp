5bad0340f4cd0e8af3fc29f7ab91392e9ef34900
LGC <lguangcong@163.com>
2025-04-25 11:40:20 +0800

    feat: add tooltip to ApplicableSwitch

--- components/pages/project/create/ApplicableSwitch.tsx
+++ components/pages/project/create/ApplicableSwitch.tsx
@@ -1,4 +1,4 @@
-import { Button, cn } from '@heroui/react';
+import { Button, cn, Tooltip } from '@heroui/react';
 import React from 'react';
 
 export interface ApplicableSwitchProps {
@@ -14,31 +14,49 @@ const ApplicableSwitch: React.FC<ApplicableSwitchProps> = ({
     'h-[26px] rounded-[8px] px-[10px] text-[14px] font-semibold min-w-0';
 
   return (
-    // TODO not applicable tooltip
-    <div className="inline-flex h-[32px] items-center gap-[5px] rounded-[10px] border border-[rgba(0,0,0,0.2)] bg-[#E1E1E1] p-[2px]">
-      <Button
-        className={cn(
-          baseClassName,
-          isApplicable
-            ? 'bg-[#464646] text-white'
-            : 'bg-transparent text-black hover:bg-[rgba(70,70,70,0.1)] active:bg-[rgba(70,70,70,0.1)]',
-        )}
-        onPress={() => onChange(true)}
-      >
-        Applicable
-      </Button>
-      <Button
-        className={cn(
-          baseClassName,
-          !isApplicable
-            ? 'bg-[#464646] text-white'
-            : 'bg-transparent text-black hover:bg-[rgba(70,70,70,0.1)] active:bg-[rgba(70,70,70,0.1)]',
-        )}
-        onPress={() => onChange(false)}
-      >
-        N/A
-      </Button>
-    </div>
+    <Tooltip
+      content={
+        <div className="w-[176px] p-[10px]">
+          <h3 className="mb-[5px] text-[13px] font-[700] text-black">
+            Not Applicable
+          </h3>
+          <p className="text-[13px] font-[400] text-black">
+            Select if the item is not applicable for this project
+          </p>
+        </div>
+      }
+      classNames={{
+        base: ['bg-transparent'],
+        content: [
+          'p-0 bg-white border border-[rgba(0,0,0,0.1)] rounded-[10px]',
+        ],
+      }}
+    >
+      <div className="inline-flex h-[32px] items-center gap-[5px] rounded-[10px] border border-[rgba(0,0,0,0.2)] bg-[#E1E1E1] p-[2px]">
+        <Button
+          className={cn(
+            baseClassName,
+            isApplicable
+              ? 'bg-[#464646] text-white'
+              : 'bg-transparent text-black hover:bg-[rgba(70,70,70,0.1)] active:bg-[rgba(70,70,70,0.1)]',
+          )}
+          onPress={() => onChange(true)}
+        >
+          Applicable
+        </Button>
+        <Button
+          className={cn(
+            baseClassName,
+            !isApplicable
+              ? 'bg-[#464646] text-white'
+              : 'bg-transparent text-black hover:bg-[rgba(70,70,70,0.1)] active:bg-[rgba(70,70,70,0.1)]',
+          )}
+          onPress={() => onChange(false)}
+        >
+          N/A
+        </Button>
+      </div>
+    </Tooltip>
   );
 };
 
