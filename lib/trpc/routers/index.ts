ff6018c71de92ddae286b37a727a7a4d10d522d9
KieSun <zx597813039@gmail.com>
2025-05-05 11:39:30 +0800

    feat: add proposalRouter to appRouter for handling proposal-related requests

--- lib/trpc/routers/index.ts
+++ lib/trpc/routers/index.ts
@@ -2,12 +2,14 @@ import { router } from '../server';
 
 import { authRouter } from './auth';
 import { projectRouter } from './project';
+import { proposalRouter } from './proposal';
 import { userRouter } from './user';
 
 export const appRouter = router({
   user: userRouter,
   auth: authRouter,
   project: projectRouter,
+  proposal: proposalRouter,
 });
 
 export type AppRouter = typeof appRouter;
