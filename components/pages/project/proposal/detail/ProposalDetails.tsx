0448749ccb2e1742810d213ea43cab8eb2197dde
LGC <lguangcong@163.com>
2025-05-15 17:04:45 +0800

    feat: opt categoryHeader ui

--- components/pages/project/proposal/detail/ProposalDetails.tsx
+++ components/pages/project/proposal/detail/ProposalDetails.tsx
@@ -575,7 +575,12 @@ const ProposalDetails = ({
 
   const renderCategoryHeader = useCallback(
     (title: string, description: string, category: CategoryKey) => (
-      <div className="flex items-center justify-between rounded-t-[10px] border border-black/10 bg-[rgba(229,229,229,0.70)] p-[10px]">
+      <div
+        className={cn(
+          'flex items-center justify-between border border-black/10 bg-[rgba(229,229,229,0.70)] p-[10px]',
+          expanded[category] ? 'rounded-t-[10px]' : 'rounded-[10px]',
+        )}
+      >
         <div className="flex flex-col gap-[5px]">
           <p className="text-[18px] font-[700] leading-[25px] text-black/80">
             {title}
