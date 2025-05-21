b514c1cc1c75ab2743283c8b42bafb708e710346
KieSun <zx597813039@gmail.com>
2025-05-21 13:30:12 +0200

    feat: enhance project filtering logic to include validation of proposal votes against accountability metrics

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,9 @@ import { POC_ITEMS } from './pocItems';
 
 export const REWARD_PERCENT = 0.2;
 export const WEIGHT = 10;
-export const ESSENTIAL_ITEM_WEIGHT_AMOUNT = 100;
+export const ESSENTIAL_ITEM_WEIGHT_AMOUNT = Object.values(POC_ITEMS)
+  .filter((item) => item.isEssential)
+  .reduce((sum, item) => sum + (item.accountability_metric || 0), 0);
 export const QUORUM_AMOUNT = 3;
 export const ESSENTIAL_ITEM_AMOUNT = Object.values(POC_ITEMS).filter(
   (item) => item.isEssential,
