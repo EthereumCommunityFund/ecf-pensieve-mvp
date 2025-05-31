b622247ff82dd8881ce415f94ccbb45a1e7d43bd
LGC <lguangcong@163.com>
2025-05-31 14:12:43 +0800

    feat: show view in not essential item

--- components/biz/table/TableColumns.tsx
+++ components/biz/table/TableColumns.tsx
@@ -427,26 +427,27 @@ const ActionsCell = ({ onView, item, itemConfig }: ActionsColCellProps) => {
   const isEssential = itemConfig?.isEssential;
   const isValueEmpty = isInputValueEmpty(item?.input);
 
-  if (!isEssential && isValueEmpty) {
-    return (
-      <Button
-        size="sm"
-        className="w-full border-none bg-[#64C0A5] text-white hover:bg-[#64C0A5]/80"
-        onPress={() => onView?.('submitPropose')}
-      >
-        Propose
-      </Button>
-    );
-  }
+  // TDOO && isValueEmpty
 
   return (
-    <Button
-      color="secondary"
-      className="h-[30px] w-full rounded-[5px] border-none bg-[#F0F0F0] p-[10px] text-[13px] font-[400]"
-      onPress={() => onView?.('viewItemProposal')}
-    >
-      View
-    </Button>
+    <div className="flex w-full gap-[10px]">
+      {!isEssential && (
+        <Button
+          size="sm"
+          className="flex-1 border-none bg-[#64C0A5] text-white hover:bg-[#64C0A5]/80"
+          onPress={() => onView?.('submitPropose')}
+        >
+          Propose
+        </Button>
+      )}
+      <Button
+        color="secondary"
+        className="h-[30px] flex-1 rounded-[5px] border-none bg-[#F0F0F0] p-[10px] text-[13px] font-[400]"
+        onPress={() => onView?.('viewItemProposal')}
+      >
+        View
+      </Button>
+    </div>
   );
 };
 
