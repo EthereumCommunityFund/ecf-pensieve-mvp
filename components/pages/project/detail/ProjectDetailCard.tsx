e14c2ad31f28d8b86472cc3177c5f1e0d66ba84e
LGC <lguangcong@163.com>
2025-06-07 23:38:05 +0800

    feat: update skeleton of project detail page

--- components/pages/project/detail/ProjectDetailCard.tsx
+++ components/pages/project/detail/ProjectDetailCard.tsx
@@ -11,42 +11,7 @@ interface ProjectDetailCardProps {
 
 const ProjectDetailCard: FC<ProjectDetailCardProps> = ({ project }) => {
   if (!project) {
-    return (
-      <div
-        className={cn(
-          'mt-[10px] mx-[20px] mobile:mx-[10px]',
-          'p-[20px] mobile:p-[14px]',
-          'bg-white border border-black/10 rounded-[10px]',
-          'flex justify-start items-start gap-[20px]',
-        )}
-      >
-        <Skeleton className="size-[100px] overflow-hidden rounded-[10px] border border-black/10" />
-
-        <div className="flex flex-1 flex-col gap-[10px]">
-          <Skeleton className="h-[25px] w-[180px]" />
-          <Skeleton className="h-[23px] w-full" />
-
-          <div className="flex flex-wrap gap-[8px]">
-            {[1, 2, 3].map((index) => {
-              return (
-                <Skeleton
-                  key={index}
-                  className="h-[22px] w-[60px] rounded-[6px]"
-                />
-              );
-            })}
-          </div>
-
-          <div className="flex items-center justify-start gap-[10px]">
-            <Skeleton className="h-[20px] w-[110px]" />
-            <Skeleton className="h-[20px] w-[16px]" />
-            <span className="text-black/20">|</span>
-            <Skeleton className="h-[20px] w-[60px]" />
-            <Skeleton className="h-[20px] w-[120px]" />
-          </div>
-        </div>
-      </div>
-    );
+    return <ProjectDetailCardSkeleton />;
   }
   return (
     <div
@@ -89,3 +54,34 @@ const ProjectDetailCard: FC<ProjectDetailCardProps> = ({ project }) => {
 };
 
 export default ProjectDetailCard;
+
+const ProjectDetailCardSkeleton = () => {
+  return (
+    <div
+      className={cn(
+        'mt-[10px] mx-[20px] mobile:mx-[10px]',
+        'p-[20px] mobile:p-[14px]',
+        'bg-white border border-black/10 rounded-[10px]',
+        'flex justify-start items-start gap-[20px]',
+      )}
+    >
+      <Skeleton className="size-[100px] shrink-0 overflow-hidden rounded-[10px] border border-black/10" />
+
+      <div className="flex min-w-0 flex-1 flex-col gap-[10px]">
+        <Skeleton className="h-[25px] w-[180px] mobile:w-[150px]" />
+        <Skeleton className="h-[23px] w-full" />
+
+        <div className="flex flex-wrap gap-[8px]">
+          {[1, 2, 3].map((index) => {
+            return (
+              <Skeleton
+                key={index}
+                className="h-[22px] w-[60px] rounded-[6px]"
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
