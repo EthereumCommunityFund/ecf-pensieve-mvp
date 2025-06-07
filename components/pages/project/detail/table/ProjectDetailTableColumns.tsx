850a3f39d8a2ec3d897d5227244094def6b9478b
LGC <lguangcong@163.com>
2025-06-07 16:40:30 +0800

    feat: update dep

--- components/pages/project/detail/table/ProjectDetailTableColumns.tsx
+++ components/pages/project/detail/table/ProjectDetailTableColumns.tsx
@@ -324,5 +324,5 @@ export const useProjectTableColumns = ({
     const actionColumns = [actionsColumn];
 
     return [...baseColumns, ...metricsColumns, ...actionColumns];
-  }, [columnHelper, isPageExpanded, showMetrics]);
+  }, [columnHelper, isPageExpanded, showMetrics, category]);
 };
