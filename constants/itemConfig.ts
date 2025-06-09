8c79d19211dea6e72efd6f7b37a64a86cd2e944c
LGC <lguangcong@163.com>
2025-06-09 23:01:34 +0800

    feat: update devStatus options

--- constants/itemConfig.ts
+++ constants/itemConfig.ts
@@ -270,6 +270,8 @@ export const AllItemConfig: Partial<Record<IItemKey, IItemConfig<IItemKey>>> = {
       { value: 'Broken / Abandoned', label: 'Broken / Abandoned' },
       { value: 'Concept', label: 'Concept' },
       { value: 'Stealth', label: 'Stealth' },
+      { value: 'Active Community', label: 'Active Community' },
+      { value: 'Operational Community', label: 'Operational Community' },
     ],
     showReference: true,
     accountability: [ALL_METRICS.TRANSPARENCY, ALL_METRICS.PERFORMANCE_EVAL],
