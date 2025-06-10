a4d536fd173063e3aa25c3509943a96d156ccc3b
LGC <lguangcong@163.com>
2025-06-10 12:14:34 +0800

    feat: update home copy, add font-800 font

--- components/pages/home/Banner.tsx
+++ components/pages/home/Banner.tsx
@@ -1,40 +1,40 @@
 import { cn } from '@heroui/react';
-
-import ECFTypography from '@/components/base/typography';
+import Link from 'next/link';
 
 const Banner = () => {
   return (
     <div
       className={cn(
-        'w-full h-[167px] mobile:h-[auto] overflow-hidden bg-white rounded-[10px] border border-[rgba(0,0,0,0.1)] p-5 relative ',
+        'w-full min-h-[167px] mobile:h-[auto] overflow-hidden bg-white rounded-[10px] border border-[rgba(0,0,0,0.1)] p-5 relative ',
         'bg-[url(/images/home/BannerBg.png)] bg-no-repeat bg-right bg-[length:auto_100%]',
         'mobile:bg-none',
       )}
     >
       <div className="z-10 size-full">
         <div className="flex h-[25px]">
-          <p className="flex h-[25px] items-center justify-end rounded-md border border-[rgba(0,0,0,0.2)] bg-[rgba(0,0,0,0.1)] px-2.5 py-1">
+          <p className="flex h-[25px] items-center justify-end rounded-[6px] border border-[rgba(0,0,0,0.2)] bg-[rgba(0,0,0,0.1)] px-2.5 py-1">
             <span className="text-[14px] font-semibold leading-[17px] tracking-[1.4]">
               ALPHA 0.1.0
             </span>
           </p>
         </div>
 
-        <ECFTypography
-          type={'title'}
-          className="mobile:text-[30px] mobile:leading-[42px] mt-2.5"
-        >
+        <p className="font-mona mobile:text-[30px] mt-[10px] text-[38px] font-[600] leading-[1.4]">
           Welcome to{' '}
-          <span className="text-[var(--primary-green)]">ecf.network,</span>{' '}
-        </ECFTypography>
+          <span className="font-[800] text-[#64C0A5]">ECF Pensieve</span>{' '}
+        </p>
 
-        <ECFTypography
-          type={'subtitle2'}
-          className="mobile:text-base mobile:leading-[26px] mt-2.5"
-        >
-          A community-curated and governed platform to discover credible web3
-          projects.
-        </ECFTypography>
+        <p className="font-mona mobile:text-[16px] mt-[10px] text-[18px] font-[500] leading-[1.6]">
+          A community-curated and governend platform based on{' '}
+          <Link
+            href="https://ethereum-community-fund.gitbook.io/the-ecf-pensieve-decentralised-social-consensus"
+            target="_blank"
+            className="font-[700] hover:underline"
+          >
+            ECF Pensieve
+          </Link>{' '}
+          for discovering accountable web3 projects and organisations.
+        </p>
       </div>
 
       <div
