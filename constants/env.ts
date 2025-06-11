4b585035fe212821292a944a073e94dd90e985af
LGC <lguangcong@163.com>
2025-06-11 09:30:05 +0800

    feat: use NEXT_PUBLIC_ENV to tell environment, and update wagmi network config

--- constants/env.ts
+++ constants/env.ts
@@ -1,4 +1,6 @@
-export const isLocalDev = process.env.NODE_ENV !== 'production';
+export const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
+export const isDev = process.env.NEXT_PUBLIC_ENV === 'dev';
+
 export const isAutoFillForm = process.env.NEXT_PUBLIC_AUTO_FILL_FORM === 'true';
 export const canScanPendingProject =
   process.env.NEXT_PUBLIC_SCAN_PENDING_PROJECT === 'true';
