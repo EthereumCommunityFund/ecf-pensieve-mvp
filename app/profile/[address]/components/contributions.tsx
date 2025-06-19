a8f3c536dc9155a41ed6f09166174d383f043a07
KieSun <zx597813039@gmail.com>
2025-06-19 12:05:54 +0800

    fix: correct import path case for ActivityItemSkeleton
    - Fix import path from './activityItemSkeleton' to './ActivityItemSkeleton'
    - Resolves Vercel build error due to case-sensitive file system
    - Local macOS filesystem is case-insensitive but git tracks correct case

--- app/profile/[address]/components/contributions.tsx
+++ app/profile/[address]/components/contributions.tsx
@@ -9,7 +9,7 @@ import dayjs from '@/lib/dayjs';
 import { trpc } from '@/lib/trpc/client';
 
 import ActivityItem from './activityItem';
-import ActivityItemSkeleton from './activityItemSkeleton';
+import ActivityItemSkeleton from './ActivityItemSkeleton';
 import { useProfileData } from './dataContext';
 
 const contributionsColorScale = scaleThreshold<number, string>()
