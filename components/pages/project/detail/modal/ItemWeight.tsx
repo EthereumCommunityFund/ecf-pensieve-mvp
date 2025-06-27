78c5350fe1c6b5fdba053cc810a7b8cccb763965
LGC <lguangcong@163.com>
2025-06-27 18:35:20 +0800

    feat: update ItemWeight label

--- components/pages/project/detail/modal/ItemWeight.tsx
+++ components/pages/project/detail/modal/ItemWeight.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 
-import { AllItemConfig } from '@/constants/itemConfig';
 import { IPocItemKey } from '@/types/item';
 
 export interface IItemWeightProps {
@@ -14,9 +13,6 @@ const ItemWeight: FC<IItemWeightProps> = ({
   itemWeight,
   itemKey,
 }) => {
-  const itemConfig = AllItemConfig[itemKey as IPocItemKey];
-  const initialItemWeight = Number(itemConfig?.weight);
-  const isBiggerThanInitialItemWeight = itemWeight > initialItemWeight;
   return (
     <div className="flex items-center gap-[10px]">
       <div className="font-mona text-[16px] font-[700] leading-[20px] text-black/80">
@@ -25,11 +21,6 @@ const ItemWeight: FC<IItemWeightProps> = ({
       <div className="rounded-[4px] border border-black/10 bg-black px-[10px] py-[5px] text-[15px] font-[600] leading-[20px] text-white">
         Weight: {itemWeight}
       </div>
-      {isBiggerThanInitialItemWeight ? (
-        <div className="text-[14px] text-black/40">
-          +{itemWeight - initialItemWeight}
-        </div>
-      ) : null}
     </div>
   );
 };
