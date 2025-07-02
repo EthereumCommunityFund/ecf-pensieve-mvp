3317c43e5cbfcb32fd9db23bcd3e6962d5f1fae5
LGC <lguangcong@163.com>
2025-07-02 20:49:42 +0800

    feat: fix type error

--- components/pages/home/HomeList.tsx
+++ components/pages/home/HomeList.tsx
@@ -72,7 +72,7 @@ const HomeList = () => {
 
   const byGenesisProjects =
     ranksData?.byGenesisWeight?.map((rank: any) => rank.project) || [];
-  const bySupportProjects = ranksData?.bySupport;
+  const bySupportProjects = ranksData?.bySupport || [];
 
   return (
     <div className="mt-5">
