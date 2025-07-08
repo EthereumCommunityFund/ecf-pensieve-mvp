339d1936a09ea3f72460c8fe2dedcf96eb313ca7
LGC <lguangcong@163.com>
2025-07-08 13:31:43 +0800

    feat: fix type

--- utils/devLog.ts
+++ utils/devLog.ts
@@ -2,7 +2,7 @@ export const devLog = (label: string, ...args: any[]) => {
   if (process.env.NODE_ENV !== 'production') {
     const color = '#7bbea6';
     console.log(
-      `%c[Penseive] ${label}`,
+      `%c[Pensieve] ${label}`,
       `background: ${color}; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;`,
       ...args,
     );
