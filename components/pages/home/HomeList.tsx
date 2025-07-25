a387a2a60edc6c5ae9b34b6ca0f7f330ba233aa9
LGC <lguangcong@163.com>
2025-07-25 21:11:13 +0800

    feat: extract SectionHeader component

--- components/pages/home/HomeList.tsx
+++ components/pages/home/HomeList.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
-import { Button } from '@/components/base/button';
 import { CommunityTrustedIcon, TagIcon } from '@/components/icons';
 import { trpc } from '@/lib/trpc/client';
 import { IProject } from '@/types';
@@ -12,52 +11,8 @@ import { formatDateWithTimeGMT } from '@/utils/formatters';
 
 import { ProjectListWrapper } from '../project/ProjectListWrapper';
 
+import SectionHeader from './SectionHeader';
 import SectionHeaderSmall from './SectionHeaderSmall';
-
-interface ISectionProps {
-  title: string;
-  description: string;
-  buttonText?: string;
-  onClick?: () => void;
-  children?: React.ReactNode;
-  updateAt?: string;
-  icon?: React.ReactNode;
-}
-
-const SectionHeader = (props: ISectionProps) => {
-  return (
-    <div className="px-[10px]">
-      <div className="mobile:flex-col mobile:items-start flex flex-1 items-center justify-between gap-[10px] py-[4px]">
-        <div>
-          <div className="flex items-center gap-[10px]">
-            {props.icon}
-            <p className="tablet:text-[18px] mobile:text-[18px] text-[24px] font-[700] leading-[1.4] text-black/80">
-              {props.title}
-            </p>
-          </div>
-
-          {props.description && (
-            <p className="mt-[5px] text-[14px] font-[400] leading-[19px] text-black/60">
-              {props.description}
-            </p>
-          )}
-        </div>
-        {props.buttonText && (
-          <div className="">
-            <Button size="sm" onPress={props.onClick} className="font-[400]">
-              {props.buttonText}
-            </Button>
-          </div>
-        )}
-      </div>
-      {props.updateAt && (
-        <p className="mt-[10px] text-left text-[10px] font-[400] leading-[14px] text-black/60">
-          {props.updateAt}
-        </p>
-      )}
-    </div>
-  );
-};
 
 const HomeList = () => {
   const router = useRouter();
