c169d30730a5b9a9f3b20519bde9e753623748a3
LienJack <longyuhuhao@126.com>
2025-08-18 22:24:09 +0900

    feat: enhance date handling across components
    - Updated date formatting in InputContentRenderer to use UTC for consistent display across timezones.
    - Modified formatProjectValue to utilize new date formatting options.
    - Improved formatDate function to support UTC formatting and added a new formatDateAsUTC function for direct UTC display.
    - Ensured consistent date handling in dateToDateValue and processDateConstraints functions by applying UTC formatting.

--- components/biz/table/utils.ts
+++ components/biz/table/utils.ts
@@ -128,8 +128,8 @@ export const formatProjectValue = (key: string, value: any): any => {
     case 'dateFounded':
     case 'dateLaunch':
       return value instanceof Date
-        ? formatDate(value)
-        : formatDate(new Date(value));
+        ? formatDate(value, 'MM/DD/YYYY', '', true)
+        : formatDate(new Date(value), 'MM/DD/YYYY', '', true);
     case 'categories':
     case 'tags':
       return Array.isArray(value) ? value.join(', ') : value;
