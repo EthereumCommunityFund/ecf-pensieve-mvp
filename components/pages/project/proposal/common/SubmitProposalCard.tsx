fede76dd1ada3ecde6fd3ec7a2b6b7e9a060a453
LGC <lguangcong@163.com>
2025-09-04 14:42:14 +0800

    feat: Small visual how-to vote on items

--- components/pages/project/proposal/common/SubmitProposalCard.tsx
+++ components/pages/project/proposal/common/SubmitProposalCard.tsx
@@ -1,7 +1,8 @@
-import { cn } from '@heroui/react';
+import { CircularProgress, cn } from '@heroui/react';
 import { FC, useMemo } from 'react';
 
 import { Button } from '@/components/base';
+import { TapHandIcon, UsersIcon, VoteIcon } from '@/components/icons';
 import { formatDate } from '@/utils/formatters';
 
 interface IProps {
@@ -49,10 +50,59 @@ const SubmitProposalCard: FC<IProps> = ({
       ) : (
         <>
           <div className="flex flex-col gap-[10px]">
-            <p className="text-[15px] font-[400] leading-[20px] text-black/80">
-              View existing proposal(s) and vote on or submit a new one for
-              review if none are accurate.
+            <p className="text-[14px] font-[400] leading-[20px] text-black/50">
+              Vote on existing proposals or submit a new one for review if none
+              are accurate.
             </p>
+            <p className="mt-[14px] text-[12px] leading-[20px] text-black/50">
+              Click this icon to vote on items:
+            </p>
+          </div>
+          <div className="relative mt-[5px] flex items-center justify-between rounded-[5px] bg-[#F8F8F8] p-[10px]">
+            <div className="flex items-center">
+              <CircularProgress
+                aria-label="Loading..."
+                color="warning"
+                showValueLabel={false}
+                size="md"
+                minValue={0}
+                maxValue={120}
+                value={120}
+                strokeWidth={8}
+                formatOptions={{
+                  style: 'decimal',
+                }}
+                classNames={{
+                  base: '',
+                  label: '',
+                  svg: 'size-[20px] rotate-[180deg]',
+                  track: 'stroke-[#D9D9D9]',
+                  indicator: 'stroke-[#64C0A5]',
+                }}
+              />
+              <span
+                className={cn(
+                  'ml-[5px] font-mona text-[13px] font-[600] leading-[19px] text-[#64C0A5]',
+                )}
+              >
+                120
+              </span>
+              <UsersIcon color={'black'} className={'ml-[10px] opacity-30'} />
+              <span
+                className={cn(
+                  'ml-[5px] font-mona text-[13px] font-[600] leading-[19px] ',
+                  'text-black/50',
+                )}
+              >
+                2/3
+              </span>
+            </div>
+
+            <VoteIcon color={'black'} className={cn('opacity-20')} />
+
+            <div className="absolute bottom-[3px] right-[3px]">
+              <TapHandIcon />
+            </div>
           </div>
           <Button onPress={onSubmitProposal}>Submit a Proposal</Button>
         </>
