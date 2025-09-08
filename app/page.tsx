bd4f3a51a6b7776283f1a8683cdcb0247bd37003
LGC <lguangcong@163.com>
2025-09-08 08:51:08 +0800

    feat: ProjectIntroCard in mobile mode

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@
 import Banner from '@/components/pages/home/Banner';
 import Categories from '@/components/pages/home/Categories';
 import HomeList from '@/components/pages/home/HomeList';
+import ProjectIntroCard from '@/components/pages/home/ProjectIntroCard';
 
 export default function Page() {
   return (
     <div className="mobile:px-[10px] px-[20px] pb-[56px] pt-[20px]">
       <Banner />
+
+      <div className="mobile:block mt-[10px] hidden">
+        <ProjectIntroCard />
+      </div>
 
       <Categories className="mt-[20px]" />
 
